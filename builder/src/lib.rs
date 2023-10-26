532cb942a8622b47de6296f1e2816b6c362b400e
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2023-10-26 12:05:05 +0200

    Builder: 01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,14 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let _ = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let expanded = quote! {
+        struct MyStruct;
+    };
+
+    expanded.into()
 }
